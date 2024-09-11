e9d2cd799273d00979cefef31970d9dacefd957a
matu.dev <matu.a.gimenez2002@gmail.com>
2024-09-10 21:57:47 -0300

    clase-12

---
+++ clase-12/script.js
@@ -0,0 +1,87 @@
+//Comentario unilinea
+/* 
+Comentario multilinea 
+*/
+
+
+/* TIPOS DE DATOS */
+
+/* OBJETOS lo vemos en otro capitulo */
+
+/* PRIMITIVOS */
+
+/* 
+STRINGS: Se escriben entre comillas ('', "", ``) y representan palabras afanumericas o textos
+BOOLEANOS BOOLEAN: son : (true false) se usan para representar estados booleanos / binarios
+NUMBERS: 7, 6.2, -4
+NULOS: null Representa ausencia de dato
+UNDEFINED: undefined Representa un dato sin definir
+*/
+
+//Strings
+/* 
+"Pepe-124"
+'Hola123'
+`Pepesito`
+'6' 
+*/
+//Booleanos
+true
+false
+//Numeros
+2
+6.2
+-4
+NaN 
+/*
+Not A Number, aparece cuando intento tranformar a un dato a numero que no se puede transformar a numero
+*/
+Infinity
+
+//Nulo
+null
+
+//undefined
+undefined
+
+//Constructor de numeros
+/* 
+Number
+
+alert(Number('-14')) 
+*/
+
+//Constructores
+/* Nos permiten pasar de un dato a otro dato */
+
+Number
+Boolean
+String
+
+//Typeof le pasamos el tipo de dato y nos devuelve como string el tipo de dato
+console.log( 
+    typeof( Number(8) ) //'number'
+)
+
+console.log(
+    typeof(Boolean(1))
+)
+
+console.log(
+    typeof(null)
+)
+
+/* 
+console.log(null == 0)
+console.log(null >= 0)
+console.log(null <= 0)
+console.log(null > 0)
+console.log(Number(null))
+ */
+
+//Consola: es una herramienta que nos permite mostrar valores de nuestro programa
+
+//console.log = registrar en consola
+console.log( 'Hola' )
+
+
